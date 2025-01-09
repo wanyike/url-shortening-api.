c000160add64c258c6e0ac1c8754dd1da5a657cd
wanyike <wanyikeemmanuel@gmail.com>
2025-01-09 23:38:47 +0300

    Update Footer.js
    update

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+          <footer className="bg-slate-800">
+              <div className="max-width">
+                  
+              </div>
+         </footer>
+          
+    </div>
+  )
+}
