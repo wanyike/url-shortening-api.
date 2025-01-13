9a539856fc5c44be13d5c601014b0980d4c89f3e
wanyike <wanyikeemmanuel@gmail.com>
2025-01-13 16:13:44 +0300

    Update Shortener.js
    created alert

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,7 +1,20 @@
+import { useState} from "react";
+
 import bgMobile from "../images/bg-shorten-mobile.svg";
 import bgDesktop from "../images/bg-shorten-desktop.svg";
 
+// https://api.shrtco.de/v2/shorten?url=
+
+
 export default function Shortener() {
+  const [text, setText] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("Input is required")
+    }
+  }
+
   return (
     <>
       <section className="max-width shortener relative">
@@ -12,10 +25,15 @@ export default function Shortener() {
           </picture>
           
    <div className="shortener-inner">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row mb-4">
-            <input className="w-full py-3 px-5 mb-2 md:mb-0 rounded-lg" type="url" placeholder="Shorten a link here" />
-            <button className="shorten-btn py-3 rounded-lg  md:w-40 md:ml-5" type="submit">
+                <input className="w-full py-3 px-5 mb-2 md:mb-0 rounded-lg" type="url" placeholder="Shorten a link here"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+                <button className="shorten-btn py-3 rounded-lg  md:w-40 md:ml-5"
+                  onClick={handleSubmit}
+                  type="submit">
               Shorten it!
             </button>
           </div>
